9e8c3e30ac95c65086bbc6b90c66202629a58480
Tracie35 <kariukitracie@gmail.com>
2022-06-23 18:03:37 +0300

    eddited search button

--- index.js
+++ index.js
@@ -2,14 +2,25 @@
 //     "https://restcountries.com/v3.1/name/${country name}?fullText=true"
 // 
 letsearchbtn = document.getElementById("search-btn");
-let countryInp =
-fetch(`https://restcountries.com/v3.1/name/${keyword}`)
-.then(res=>res.json())
-.then(data=>
+let countryInp =document.getElementById
+("country-inp");
+searchBtn.addeventListener("click",()=>{
+    let countryName = "india";
+    let finalURL = `https://restcountries.com/v3.1/name/${keyword}`
+    console.log (finalURL)
+    fetch(`https://restcountries.com/v3.1/name/${keyword}`)
+.then((response) => response.json())
+.then((data) => {
+    console.log (data[0]);
+    console.log ()
+
+}
+})
+
     
     
     
-    console.log (typeof(data)))
+
 
 
 
