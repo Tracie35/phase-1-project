286096224e3f10d913a66b50500dc057ebb456d2
Tracie35 <kariukitracie@gmail.com>
2022-06-23 15:25:16 +0300

    add fetch data

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // .get.JSON {
 //     "https://restcountries.com/v3.1/name/${country name}?fullText=true"
 // 
-
-fetch('https://restcountries.com/v3.1/name/Uganda')
+letsearchbtn = document.getElementById("search-btn");
+let countryInp =
+fetch('https://restcountries.com/v3.1/name/China')
 .then(res=>res.json())
 .then(data=>console.log(data))
+
+
+
 
